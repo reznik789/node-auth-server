5395f8ca58086634e39d488d91c4bfa6cac177bd
Alex Reznichenko <alex.reznichenko789@gmail.com>
2017-10-28 12:50:39 +0300

    Base Server implemintation

---
+++ models/user.js
@@ -0,0 +1,42 @@
+const mongoos = require('mongoose');
+const Schema = mongoos.Schema;
+const bcrypt = require('bcrypt-nodejs');
+
+//Define our module
+let userSchema = new Schema({
+    email: {
+        type: String,
+        unique: true,
+        lowercase: true
+    },
+    password: String
+});
+
+userSchema.pre('save', function (next) {
+    let user = this;
+    bcrypt.genSalt(10, function (err, salt) {
+        if (err) {
+            return next(err);
+        }
+        bcrypt.hash(user.password, salt, null, function (err, hash) {
+            if (err) { return next(err) };
+            user.password = hash;
+            console.log(hash);
+            next();
+        })
+    });
+});
+
+userSchema.methods.comparePassword = function (candidatePassword, callback) {
+    bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+        if (err) { return callback(err); }
+
+        callback(null, isMatch);
+    });
+};
+
+//Create the model class
+const ModelClass = mongoos.model('user', userSchema);
+
+//Export the model
+module.exports = ModelClass;
