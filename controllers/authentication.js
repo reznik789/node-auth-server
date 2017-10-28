65b7f2cc20690ce5bfc44886a3909b656f956c40
Alex Reznichenko <alex.reznichenko789@gmail.com>
2017-10-28 12:43:23 +0300

    Base Server implemintation

---
+++ controllers/authentication.js
@@ -0,0 +1,51 @@
+const User = require("../models/user");
+const jwt = require("jwt-simple");
+const config = require("../config");
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
+
+exports.signup = function(req, res, next) {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(422).send({
+      error: "You must provide email and password"
+    });
+  }
+
+  User.findOne({ email: email }, function(err, existingUser) {
+    if (err) {
+      return next(err);
+    }
+    if (existingUser) {
+      res.status(422).send({
+        error: "Email is in use"
+      });
+    }
+    const user = new User({
+      email: email,
+      password: password
+    });
+    user.save(function(err) {
+      if (err) {
+        return next(err);
+      }
+      res.send({
+        success: true,
+        token: tokenForUser(user)
+      });
+    });
+  });
+};
+
+exports.signin = function(req, res, next) {
+  User.findOne({ email: req.email }, function(err, user) {
+    res.send({
+      success: true,
+      token: tokenForUser(req.user)
+    });
+  });
+};
